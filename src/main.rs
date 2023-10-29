da5a39c8cc5a6ed4dadf17a2f019af054c04e471
vogonwann <wannjanjic@gmail.com>
2023-10-29 14:06:09 +0100

    Update quote handler added

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod handlers;
-use axum::routing::{get, post, Router};
+use axum::routing::{get, post, put, Router};
 use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
@@ -18,6 +18,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/", get(handlers::health))
         .route("/quotes", post(handlers::create_quote))
         .route("/quotes", get(handlers::read_quotes))
+        .route("/quotes/:id", put(handlers::update_quote))
         .with_state(pool);
 
     axum::Server::bind(&addr.parse().unwrap())
